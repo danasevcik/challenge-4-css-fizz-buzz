cb0de5b34909a0a70c598f9642eca5ef5f87286d
github username <danasevcik@hotmail.com>
2019-06-27 10:00:17 -0400

    working

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ window.addEventListener('DOMContentLoaded', (e) => {
   function changeColor() {
     let tableBody = document.querySelector('#table-body')
     let tableArr = Array.from(tableBody.children)
-    tableArr.forEach(row => {
-      console.log(row);
-    })
+    for(let i = 0; i < tableArr.length; i++) {
+      console.log(tableArr[i]);
+      if (i % 3 === 0) {
+        console.log(tableArr[i]);
+        tableArr[i].setAttribute("class", "colorOne")
+      } else if (i % 5 === 0) {
+        tableArr[i].setAttribute('class', 'colorTwo')
+      }
+    }
+    console.log(tableArr);
   }
 
   changeColor()
